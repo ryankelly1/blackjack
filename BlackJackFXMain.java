0d3b671424e0d079b66474c320a6f2b270277180
ryankelly1 <49239869+ryankelly1@users.noreply.github.com>
2019-04-03 11:17:03 -0400

    Update BlackJackFXMain.java

--- BlackJackFXMain.java
+++ BlackJackFXMain.java
@@ -7,19 +7,7 @@ public class BlackJackFXMain {
     
       public static void main(String args[]) {
           BlackJackController controller = new BlackJackController();
-          
-          // Test Requirements: new game button will deal cards and have a working hit button.
-          // The goals of this deliverable were to get the cards dealt for the player.
-          // This goal was accomplished, along with the addition of a working hit button. 
-
-          // The MVC diagram of this program is listed below
-          // Main Method: BlackJackFXMain
-          // View: BlackJackFX
-          // Models: Card, Dealer and Deck
-          
-          
-
-       
+               
     }
     
 }
